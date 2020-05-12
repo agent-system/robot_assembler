f77e6cb56a6d5d1d5ecab63dab2e631a954fcf70
Yohei Kakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2020-05-13 08:41:31 +0900

    add scripts

---
+++ scripts/create_model_with_sensors.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+URDF_FILE=$1
+ROBOT_NAME=$(basename ${URDF_FILE} .urdf)
+XACRO_FILE=${URDF_FILE}.xacro
+URDF_URI=''
+
+if [ $# -gt 1 ]; then
+    package_name=$2
+    target_dir=$(rospack find ${package_name})
+    URDF_URI="\$(find ${package_name})/${URDF_FILE}"
+    URDF_FILE=${target_dir}/${URDF_FILE}
+    XACRO_FILE=${target_dir}/${XACRO_FILE}
+    echo $URDF_URI
+fi
+
+YAML_FILE=${URDF_FILE}.euscollada.yaml
+EUS_FILE=${URDF_FILE}.l
+
+rosrun euscollada collada2eus -I ${URDF_FILE} -C ${YAML_FILE} -O ${EUS_FILE}
+
+if [ URDF_URI == '' ]; then
+    rosrun robot_assembler create_model_with_sensors.py -N ${ROBOT_NAME} -Y ${YAML_FILE} -U ${URDF_FILE} -O ${XACRO_FILE}
+else
+    rosrun robot_assembler create_model_with_sensors.py -N ${ROBOT_NAME} -Y ${YAML_FILE} -U "${URDF_URI}" -O ${XACRO_FILE}
+fi
+
+#cat<<EOF >> ${EUS_FILE}
+#(when (and (boundp 'euscollada-robot) (class euscollada-robot))
+#  (let ((clst (remove-if-not #'(lambda (cl) (eq (send cl :super) euscollada-robot))
+#                             (system::list-all-classes))))
+#    (when (= (length clst) 1)
+#      (setq *assembled-robot-class* (car clst)))
+#))
+#EOF
