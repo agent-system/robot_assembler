b87b694e04d49657a780a45ac904794d810d86d6
Yohei Kakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2021-04-26 15:21:54 +0900

    add code for parsing extra-data

--- euslisp/compile.sh
+++ euslisp/compile.sh
@@ -2,10 +2,11 @@
 
 set -e
 
+irteusgl "(progn (setq *error-handler* #'(lambda (&rest args) (unix::exit -1))) (comp::compile-file \"eus-yaml.l\") (exit))"
 irteusgl "(progn (setq *error-handler* #'(lambda (&rest args) (unix::exit -1))) (comp::compile-file \"robot-assembler.l\") (exit))"
 irteusgl "(progn (setq *error-handler* #'(lambda (&rest args) (unix::exit -1))) (comp::compile-file \"robot-assembler-utils.l\") (exit))"
 irteusgl "(progn (setq *error-handler* #'(lambda (&rest args) (unix::exit -1))) (comp::compile-file \"robot-assembler-viewer.l\") (exit))"
 
 #rm -f robot-assembler.{o,c,h,so} robot-assembler-utils.{o,c,h,so} robot-assembler-viewer.{o,c,h,so}
 #rm -f robot-assembler.so robot-assembler-utils.so robot-assembler-viewer.so
-rm -f robot-assembler.{o,c,h} robot-assembler-utils.{o,c,h} robot-assembler-viewer.{o,c,h}
+rm -f robot-assembler.{o,c,h} robot-assembler-utils.{o,c,h} robot-assembler-viewer.{o,c,h} eus-yaml.{o,c,h}
